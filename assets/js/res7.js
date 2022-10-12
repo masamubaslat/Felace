c6eb2d41137b19edd4e90c284d0efb288ebfd97e
masamubaslat <masa.mubaslat111#gmail.com>
2022-10-12 23:10:42 +0300

    v3

--- assets/js/res7.js
+++ assets/js/res7.js
@@ -10,6 +10,17 @@ let s1=[
     {id :8 , image : 'assets/css/images/res7/s1-8.jpg' , tilte:'Fullscreen Showcase' ,type:'resturant'},
     {id :9 , image : 'assets/css/images/res7/s1-9.jpg' , tilte:'Carousel Showcase' , type:'cafe'},  
 ]
+let s3=[
+    {id :1 , image : 'assets/css/images/res7/s3-1.jpg' , tilte:'Meet The Chef' , type:'cafe'},
+    {id :2 , image : 'assets/css/images/res7/s3-2.jpg' , tilte:'Our Menu' , type:'cafe' },
+    {id :3 , image : 'assets/css/images/res7/s3-3.jpg' , tilte:'Gallery Page', type:'cafe'},
+    {id :4 , image : 'assets/css/images/res7/s3-4.jpg' , tilte:'About Us' , type:'resturant'},
+    {id :5 , image : 'assets/css/images/res7/s3-5.jpg' , tilte:'Contact Us' , type:'cafe'},
+    {id :6 , image : 'assets/css/images/res7/s3-6.jpg' , tilte:'Booking Page' , type:'cafe'},
+    {id :7 , image : 'assets/css/images/res7/s3-7.jpg' , tilte:'Our Kitchen' , type:'resturant'},
+    {id :8 , image : 'assets/css/images/res7/s3-8.jpg' , tilte:'Our Chefs' ,type:'resturant'},
+    {id :9 , image : 'assets/css/images/res7/s3-9.jpg' , tilte:'Coming Soon' , type:'cafe'},  
+]
 
 function home_pages(arr, place){
     let list =" ";
@@ -18,14 +29,17 @@ function home_pages(arr, place){
         list+=`
                <div class="mt-5 text-center ">
                             <div class="s1-1" style="background:url(${arr[i].image}); background-size:cover";></div>
-                            <p class="mt-3 w-50">${arr[i].tilte}</p>
+                            <p class="mt-3 text-white">${arr[i].tilte}</p>
                          </div>
                    
                 `
     }
     document.getElementById(place).innerHTML=list;
 }
-home_pages(s1, 'res1-data1');          
+home_pages(s1, 'res1-data1');      
+home_pages(s3, 'res7-data2'); 
+//https://forkify-api.herokuapp.com/api/search?q=seafood
+
 //****************************************** end main resturant page ********************************************
 
 
